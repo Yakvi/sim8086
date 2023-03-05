be7800acd1764550a340fb9a85165d91e2c1f846
Yakvi <ivankoy@gmail.com>
2023-03-05 18:35:07 +0100

    Project setup

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace decoder8086
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
